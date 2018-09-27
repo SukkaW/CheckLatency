37ed482c30b323655780ebff2047a06c1bcaf6fd
SukkaW <isukkaw@gmail.com>
2018-09-27 22:50:36 +0800

    feat(app): get server provider name

---
+++ src/app.js
@@ -0,0 +1,22 @@
+/*
+ * Fetch helper
+ */
+get = (url) =>
+    fetch(url, {
+        method: 'GET'
+    }).then(resp => Promise.all([resp.ok, resp.status, resp.json()])
+    ).then(([ok, status, json]) => {
+        if (ok) {
+            return json;
+        } else {
+            throw new Error(JSON.stringify(json.error));
+        }
+    })/*.catch(error => {
+        throw error;
+    })*/;
+
+get("server.json").then(json => {
+    Object.keys(json).forEach((value, index) => {
+        console.log(value);
+    });
+});
