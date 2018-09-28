103965859dafc273cfbfc54e55282dcc88e5a901
SukkaW <isukkaw@gmail.com>
2018-09-28 11:10:30 +0800

    refactor: card uiux

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ get("server.json").then(json => {
         Object.keys(region).forEach((value, index) => {
             var name = value;
             var region_data = region[value];
-            var domain = region_data.link;
+            var domain = region_data.link.replace('http://', '').replace('https://', '');
             if (region_data.dl) {
                 var testfile = '<div class="card-footer sk-pt-1">';
                 for(i in region_data.dl) {
@@ -36,7 +36,7 @@ get("server.json").then(json => {
             }
             var cloudItem = `
             <div class="column col-xs-12 col-md-6 col-xl-4 col-3 sk-pt-4">
-                <div class="card">
+                <div class="card sk-shadow-1">
                     <div class="card-header">
                         <div class="card-title text-bold h5">${name}</div>
                         <div class="card-subtitle text-gray sk-text-small">${domain}</div>
@@ -45,11 +45,6 @@ get("server.json").then(json => {
                         <meter class="meter" value="20" min="0" max="100"></meter>
                         <p class="text-center sk-text-dark mb-0"><small>100ms</small></p>
                     </div>
-                    <!--
-                    
-                        <a href="#" class="sk-mr-2">10G</a><a href="#">100G</a>
-                    </div>
-                    -->
                     ${testfile}
                 </div>
             </div>
