2e084811c9428decf505b2fe379cfd46c3df17cc
SukkaW <isukkaw@gmail.com>
2018-09-28 11:43:10 +0800

    feat: add EventListener for check button

--- src/app.js
+++ src/app.js
@@ -14,27 +14,27 @@ get = (url) =>
     })/*.catch(error => {
         throw error;
     })*/;
-
-get("server.json").then(json => {
-    Object.keys(json).forEach((value, index) => {
-        var html = '<h2 class="h4 sk-text-dark sk-mt-8 sk-mb-2 sk-text-bold">' + value + '</h2>';
-        html += '<div class="columns">';
-        var region = json[value];
-        Object.keys(region).forEach((value, index) => {
-            var name = value;
-            var region_data = region[value];
-            var domain = region_data.link.replace('http://', '').replace('https://', '');
-            if (region_data.dl) {
-                var testfile = '<div class="card-footer sk-pt-1">';
-                for(i in region_data.dl) {
-                    var testItem = '<a href="' + domain + region_data.dl[i] + '" class="sk-mr-2">' + i + '</a>';
-                    testfile += testItem;
+function renderCard() {
+    get("server.json").then(json => {
+        Object.keys(json).forEach((value, index) => {
+            var html = '<h2 class="h4 sk-text-dark sk-mt-8 sk-mb-2 sk-text-bold">' + value + '</h2>';
+            html += '<div class="columns">';
+            var region = json[value];
+            Object.keys(region).forEach((value, index) => {
+                var name = value;
+                var region_data = region[value];
+                var domain = region_data.link.replace('http://', '').replace('https://', '');
+                if (region_data.dl) {
+                    var testfile = '<div class="card-footer sk-pt-1">';
+                    for (i in region_data.dl) {
+                        var testItem = '<a href="' + domain + region_data.dl[i] + '" class="sk-mr-2">' + i + '</a>';
+                        testfile += testItem;
+                    }
+                    testfile += '</div>'
+                } else {
+                    var testfile = '';
                 }
-                testfile += '</div>'
-            } else {
-                var testfile = '';
-            }
-            var cloudItem = `
+                var cloudItem = `
             <div class="column col-xs-12 col-md-6 col-xl-4 col-3 sk-pt-3">
                 <div class="card sk-shadow-1">
                     <div class="card-header">
@@ -49,10 +49,24 @@ get("server.json").then(json => {
                 </div>
             </div>
             `;
-            html += cloudItem;
+                html += cloudItem;
+            });
+            html += '</div>';
+            html += '<button id="' + value + '" class="btn btn-primary sk-mt-3 check-latency-button">开始测试</button>';
+            document.getElementById('app').insertAdjacentHTML('beforeend', html);
         });
-        html += '</div>';
-        html += '<button class="btn btn-primary sk-mt-3">开始测试</button>';
-        document.getElementById('app').insertAdjacentHTML('beforeend', html);
+    }).then(function() {
+        var $btn = document.getElementsByClassName('check-latency-button');
+        for (var i = 0; i < $btn.length; i += 1) {
+            $btn[i].addEventListener('click', owo);
+        };
     });
-});
+}
+
+renderCard();
+
+function owo() {
+    console.log(this.getAttribute('id'))
+}
+
+
